d4f757d932beeab7450f018588c07ae2c2e93526
gazayas <g-zayas@hotmail.com>
2023-04-02 15:28:36 +0900

    Update Song factory for testing

--- test/factories/songs.rb
+++ test/factories/songs.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :song do
     association :team
-    title { "MyString" }
-    key { "MyString" }
-    author { "MyString" }
-    body { "MyText" }
-    video_url { "MyString" }
+    title { "その日全世界が" }
+    key { "A" }
+    author { "長沢崇文" }
+    body { "その日全世界が主の御名　高く掲げる　叫べ、王の王イエスに" } # TODO: コードを入れてシステムテストを行いたい。
+    video_url { "MyString" } # TODO
   end
 end
